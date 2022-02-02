7f597fd2db4bf0538f27c2c5ec23f2582c27997d
Rasa Salakij <r.salakij@adinstruments.com>
2022-02-03 11:27:18 +1300

    Create items without statuses

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([{name: ''}]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Kanbanboard</p>
       </header>
+      <CreateItemForm items={items} setItems ={setItems}/>
+      {/* <LooseItemsBox items={items} setItems ={setItems}/> */}
     </div>
   );
 }
 
 export default App;
+
+type itemsType = {name: string}[]
+interface createItemProps  {
+  items:itemsType; 
+  setItems: (i:itemsType)=>void ;
+}
+const CreateItemForm = ((props:createItemProps) => {
+  const {items,setItems} = props;
+ const [itemName,setItemName] = React.useState('');
+  
+  const handleSubmit = (evt:any) => {
+      evt.preventDefault();
+      items.push({name: itemName});
+      console.log(items);
+      setItems(items);
+      setItemName('');   
+  }
+  return (
+    <>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Item: 
+        <input
+          type="text"
+          value={itemName}
+          onChange={e =>(setItemName(e.target.value))}
+        />
+      </label>
+      <button type="submit" value="Create" >Create</button>
+    </form>
+    {/* <span>{itemName}</span> */}
+    {items.map((item, index)=>{
+      return(
+      <div>
+       { `item ${index} : ${item.name}`}
+      </div>
+      )
+    })}
+    </>
+  );
+});
+
+const LooseItemsBox = ((props:{
+  items:itemsType; 
+  setItems: (i:itemsType)=>void ;
+})=> {
+  const {items,setItems} = props;
+  return (
+    <>
+    {items.map((item)=>{
+      <div>
+        {item.name}
+      </div>
+    })}
+  </>
+  );
+});
